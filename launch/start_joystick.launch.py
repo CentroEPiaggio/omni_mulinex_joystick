811c25a45e2092cfe517429316f9761b5f4ecbf0
LoreMarti91 <martignettilorenzo@gmail.com>
2026-03-27 16:47:42 +0000

    fix joystick launch: path + rosbag config

--- launch/start_joystick.launch.py
+++ launch/start_joystick.launch.py
@@ -13,19 +13,25 @@ def generate_launch_description():
     joy_node_path = get_package_share_path("omni_mulinex_joystick")
 
     joy_cfg_file = os.path.join(joy_node_path,"config","joy_node.yaml")
-    bag_name = f"bag_{datetime.now().strftime('%Y%m%d_%H%M%S')}"
+    # bag_name = f"bag_{datetime.now().strftime('%Y%m%d_%H%M%S')}"
+
+    bag_dir = os.path.expanduser("~/mulinex_ws/bag")
+    os.makedirs(bag_dir, exist_ok=True)
+
+    timestamp = datetime.now().strftime('%Y_%m_%d_%H_%M_%S')
+    bag_path = os.path.join(bag_dir, f"Test_joystick_{timestamp}")
 
     subprocess.check_output(
         ["ros2 control load_controller omni_controller --set-state active "]
         ,shell=True)
-    
-    subprocess.check_output(
-        ["ros2 control load_controller state_broadcaster --set-state active"]
-        ,shell=True)
+    # COmmentare i 2 controllori sotto se utilizzi interfaccia new_mulsbc_ws
+    # subprocess.check_output(
+    #     ["ros2 control load_controller state_broadcaster --set-state active"]
+    #     ,shell=True)
 
-    subprocess.check_output(
-        ["ros2 control load_controller distributor_state_broadcaster --set-state active"]
-        ,shell=True)
+    # subprocess.check_output(
+    #     ["ros2 control load_controller distributor_state_broadcaster --set-state active"]
+    #     ,shell=True)
 
 
     joy_event_node = Node(
@@ -42,7 +48,8 @@ def generate_launch_description():
     )
 
     bag_record = ExecuteProcess(
-        cmd=["ros2", "bag", "record", "-a", "-s", "mcap", "-o", bag_name],
+        # cmd=["ros2", "bag", "record", "-a", "-s", "mcap", "-o", bag_name],
+        cmd=["ros2", "bag", "record", "-a", "-s", "mcap", "-o", bag_path],
         output="screen"
     )
     
